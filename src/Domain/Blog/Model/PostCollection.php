bb71abf19bbe795ff7da4e1d1a74eb04afba4543
Jose Suarez <chua.jose@gmail.com>
2024-03-06 23:32:49 +0100

    [add] Collection to Post

---
+++ src/Domain/Blog/Model/PostCollection.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Blog\Model;
+
+class PostCollection
+{
+    /**
+     * @param array<int, Post> $items
+     */
+    public function __construct(private array $items)
+    {
+        foreach ($items as $item) {
+            if (!$item instanceof Post) {
+                throw new \InvalidArgumentException('The object is not an instance of Post', 500);
+            }
+        }
+    }
+
+    /** @return \ArrayIterator<int, Post> */
+    public function getIterator(): \ArrayIterator
+    {
+        return new \ArrayIterator($this->items());
+    }
+
+    public function count(): int
+    {
+        return count($this->items());
+    }
+
+    /** @return array<int, Post> */
+    protected function items(): array
+    {
+        return $this->items;
+    }
+
+    public function add(Post $item): void
+    {
+        $this->items[] = $item;
+    }
+
+    public function clear(): void
+    {
+        $this->items = [];
+    }
+}
