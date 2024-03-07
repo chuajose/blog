cee7bacfa1f68941ce0078a50cf35da6c1978374
Jose Suarez <chua.jose@gmail.com>
2024-03-07 13:43:09 +0100

    [add] Messenger to send events

---
+++ src/Domain/Shared/Aggregate/AggregateRoot.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Shared\Aggregate;
+use App\Domain\Shared\Event\Event;
+
+
+abstract class AggregateRoot
+{
+
+    /**
+     * @var array<int, Event>
+     */
+    private array $domainEvents = [];
+
+    /**
+     * @return array<int, Event>
+     */
+    final public function pullDomainEvents(): array
+    {
+        $domainEventsPull = $this->domainEvents;
+        $this->domainEvents = [];
+        return $domainEventsPull;
+    }
+
+    final public function record(Event $domainEvent): void
+    {
+        $this->domainEvents[] = $domainEvent;
+    }
+
+    /**
+     * @return array<int, Event>
+     */
+    final  public function getEvents(): array
+    {
+
+        return $this->domainEvents;
+    }
+}
---
+++ src/Domain/Shared/Event/Event.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Shared\Event;
+
+interface Event {
+    /**
+     * @return array<string, int|string>
+     */
+    public function payload(): array;
+    public function eventName(): string;
+}
