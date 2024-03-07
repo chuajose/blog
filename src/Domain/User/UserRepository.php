1d2a0774b5be27d2a46cc72ddc6e28cc8079e1db
Jose Suarez <chua.jose@gmail.com>
2024-03-07 20:10:14 +0100

    [update] User repository

--- src/Domain/User/UserRepository.php
+++ src/Domain/User/UserRepository.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Domain\User;
 
+use App\Domain\Shared\Criteria\Criteria;
 use App\Domain\User\Model\User;
+use App\Domain\User\Model\UserCollection;
 use Symfony\Component\Uid\Uuid;
 
 interface UserRepository
@@ -12,7 +14,8 @@ interface UserRepository
 
     public function find(Uuid $id): ?User;
 
+    public function all(Criteria $criteria): UserCollection;
+
     public function create(User $user): void;
 
-    public function delete(int $id): void;
 }
--- src/Infrastructure/Persistence/Doctrine/User/DoctrineOrmUserRepository.php
+++ src/Infrastructure/Persistence/Doctrine/User/DoctrineOrmUserRepository.php
@@ -2,15 +2,46 @@
 
 namespace App\Infrastructure\Persistence\Doctrine\User;
 
+use App\Domain\Shared\Criteria\Criteria;
 use App\Domain\User\Model\User;
+use App\Domain\User\Model\UserCollection;
 use App\Domain\User\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Component\Uid\Uuid;
 
 readonly class DoctrineOrmUserRepository implements UserRepository
 {
     public function __construct(private EntityManagerInterface $entityManager)
     {
+    }
+
+    /**
+     * @param QueryBuilder $dql
+     * @param int $page
+     * @param int $limit
+     * @return Paginator<User>
+     */
+    private function paginate(QueryBuilder $dql, int $page = 1, int $limit = 1): Paginator
+    {
+        $paginator = new Paginator($dql);
+
+        $paginator->getQuery()
+            ->setFirstResult($limit * ($page - 1)) // Offset
+            ->setMaxResults($limit); // Limit
+
+        return $paginator;
+    }
+
+    public function all(Criteria $criteria): UserCollection
+    {
+        $query = $this->entityManager->createQueryBuilder();
+        $query->select('u')
+            ->from(User::class, 'u');
+
+        $paginator = $this->paginate($query, $criteria->offset()??1, $criteria->limit()??10);
+        return new UserCollection($paginator->getQuery()->getResult(), $paginator->count());
     }
 
     public function find(Uuid $id): ?User
@@ -24,8 +55,4 @@ readonly class DoctrineOrmUserRepository implements UserRepository
         $this->entityManager->flush();
     }
 
-    public function delete(int $id): void
-    {
-        // TODO: Implement delete() method.
-    }
 }
